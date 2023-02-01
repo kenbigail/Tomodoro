c60521f29d6bd46c373c44220bbcd645d51cdd35
Keenan Abigail <keenanabigailidn@gmail.com>
2023-02-01 10:48:52 +0700

    Moodboard Progress (Keenan)

---
+++ app/src/main/java/com/pendekarsoftware/tomodoro/MoodboardActivity.kt
@@ -0,0 +1,11 @@
+package com.pendekarsoftware.tomodoro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoodboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moodboard)
+    }
+}
